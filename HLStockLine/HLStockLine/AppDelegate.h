a9c5880ade44aee42dad02ca4b3742e63e9bbc7f
HomerIce <wyh900207@126.com>
2019-04-24 12:04:34 +0800

    init

---
+++ HLStockLine/HLStockLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLStockLine
+//
+//  Created by wyh on 2019/4/24.
+//  Copyright © 2019 wyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLStockLine/HLStockLine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLStockLine
+//
+//  Created by wyh on 2019/4/24.
+//  Copyright © 2019 wyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
