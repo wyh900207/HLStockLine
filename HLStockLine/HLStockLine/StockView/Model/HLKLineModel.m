191baf07d285051b451d2c81c904f8d520720c1b
HomerIce <wyh900207@126.com>
2019-04-24 18:15:14 +0800

    KLineModel.h

--- HLStockLine/HLStockLine/StockView/Model/HLKLineModel.m
+++ HLStockLine/HLStockLine/StockView/Model/HLKLineModel.m
@@ -10,4 +10,60 @@
 
 @implementation HLKLineModel
 
+- (NSNumber *)MA12
+{
+    if (!_MA12) {
+        NSInteger index = [self.ParentGroupModel.models indexOfObject:self];
+        if (index >= 11) {
+            if (index > 11) {
+                _MA12 = @((self.SumOfLastClose.floatValue - self.ParentGroupModel.models[index - 12].SumOfLastClose.floatValue) / 12);
+            } else {
+                _MA12 = @(self.SumOfLastClose.floatValue / 12);
+            }
+        }
+    }
+    return _MA12;
+}
+
+- (NSNumber *)MA26
+{
+    if (!_MA26) {
+        NSInteger index = [self.ParentGroupModel.models indexOfObject:self];
+        if (index >= 25) {
+            if (index > 25) {
+                _MA26 = @((self.SumOfLastClose.floatValue - self.ParentGroupModel.models[index - 26].SumOfLastClose.floatValue) / 26);
+            } else {
+                _MA26 = @(self.SumOfLastClose.floatValue / 26);
+            }
+        }
+    }
+    return _MA26;
+}
+
+- (NSNumber *)DIF
+{
+    if(!_DIF) {
+        _DIF = @(self.EMA12.floatValue - self.EMA26.floatValue);
+    }
+    return _DIF;
+}
+
+//已验证
+-(NSNumber *)DEA
+{
+    if(!_DEA) {
+        _DEA = @(self.PreviousKlineModel.DEA.floatValue * 0.8 + 0.2*self.DIF.floatValue);
+    }
+    return _DEA;
+}
+
+//已验证
+- (NSNumber *)MACD
+{
+    if(!_MACD) {
+        _MACD = @(2*(self.DIF.floatValue - self.DEA.floatValue));
+    }
+    return _MACD;
+}
+
 @end
